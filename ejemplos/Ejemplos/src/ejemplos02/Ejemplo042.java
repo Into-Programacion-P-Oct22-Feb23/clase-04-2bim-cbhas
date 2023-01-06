5690894efbc05bf2cbfb8f8e09bf52e10959fff7
Sebastián <calderonsebastian211@gmail.com>
2023-01-06 12:52:20 -0500

    Adelanto

---
+++ ejemplos/Ejemplos/src/ejemplos02/Ejemplo042.java
@@ -0,0 +1,44 @@
+package ejemplos02;
+
+public class Ejemplo042 {
+
+    public static void main(String[] args) {
+
+        int[] informacion = {1, 2, 3, 10, 20, 30, 100, 200, 300};
+
+        System.out.printf("La media aritmética es: %.0f\n",
+                mediaAritmetica(informacion));
+        System.out.printf("La desviación estándar es: %.2f\n",
+                desviacionEstandar(informacion));
+
+    }
+
+    public static double mediaAritmetica(int[] a) {
+
+        double suma = 0;
+        double media = 0;
+
+        for (int i = 0; i < a.length; i++) {
+            suma = suma + a[i];
+        }
+        media = suma / a.length;
+
+        return media;
+    }
+
+    public static double desviacionEstandar(int[] a) {
+
+        double suma = 0;
+        double media = mediaAritmetica(a);
+        double numerador = 0;
+        double desviacion;
+
+        for (int i = 0; i < a.length; i++) {
+
+            numerador = numerador + Math.pow(a[i] - media, 2);
+
+        }
+        desviacion = Math.sqrt(numerador / a.length);
+        return desviacion;
+    }
+}
